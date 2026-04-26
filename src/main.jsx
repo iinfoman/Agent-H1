806442b50dc1457a7f66e1af5ff54e7cc14ea9e8
iinfoman <iinfoworks@gmail.com>
2026-04-26 23:25:46 +0200

    Initialize main.jsx with React and AgentH1 component

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AgentH1 from './AgentH1'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AgentH1 />
+  </React.StrictMode>
+)
